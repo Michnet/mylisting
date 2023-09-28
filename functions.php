7f4ed3288250262e1aa98ea7e1eabc7da909239d
Michnet <ntumichael@gmail.com>
2023-09-28 11:24:42 +0300

    array

--- functions.php
+++ functions.php
@@ -1436,7 +1436,7 @@ class MY_REST_Comments_Controller extends WP_REST_Comments_Controller {
     $response = parent::get_items($request);
     $params = $request->get_params();
 
-    $prepared_args = array('comment__in' => $request['include'], 'comment__not_in' => $request['exclude'], 'number' => $request['per_page'], 'post_id' => $request['post'] ? $request['post'] : '', 'parent' => isset($params['parent']) ? intval($params['parent']) : '', 'search' => $request['search'], 'offset' => $request['offset'], 'orderby' => $this->normalize_query_param($request['orderby']), 'order' => $request['order'], 'status' => 'approve', 'type' => 'comment', 'no_found_rows' => false, 'hierarchical' => 'threaded');
+    $prepared_args = array('comment__in' => $request['include'], 'comment__not_in' => $request['exclude'], 'number' => $request['per_page'], 'post_id' => $request['post'] ? $request['post'] : '', 'parent' => isset($params['parent']) ? intval($params['parent'][0]) : '', 'search' => $request['search'], 'offset' => $request['offset'], 'orderby' => $this->normalize_query_param($request['orderby']), 'order' => $request['order'], 'status' => 'approve', 'type' => 'comment', 'no_found_rows' => false, 'hierarchical' => 'threaded');
     if (empty($request['offset'])) {
         $prepared_args['offset'] = $prepared_args['number'] * (absint($request['page']) - 1);
     }
