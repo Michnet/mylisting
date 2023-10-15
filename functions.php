ae490be8c5faf25a54406418e9ea31e07db28033
Michnet <ntumichael@gmail.com>
2023-10-15 16:34:23 +0300

    float

--- functions.php
+++ functions.php
@@ -1843,7 +1843,7 @@ add_action( 'rest_api_init', 'add_term_meta_rest' );
         $meta_0bj = new stdClass();
         $meta = get_term_meta( $term['id'] );
         $meta_0bj->icon_image_url = $meta['icon_image'][0] ? wp_get_attachment_url(number_format($meta['icon_image'][0])) : null;
-        $meta_0bj->image_url = $meta['image'][0] ? wp_get_attachment_url(number_format($meta['image'])) : null;
+        $meta_0bj->image_url = $meta['image'][0] ? wp_get_attachment_url(number_format($meta['image'][0])) : null;
         $meta_0bj->color = $meta['color'][0]  ?? null;
       	$meta_0bj->icon = $meta['icon'][0]  ?? null;
         $meta_0bj->rl_awesome = $meta['rl_awesome'][0]  ?? null;
