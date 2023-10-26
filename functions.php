266fc9f222a443db45df6c74a2442cdb44064f32
Michnet <ntumichael@gmail.com>
2023-10-26 13:23:00 +0300

    try

--- functions.php
+++ functions.php
@@ -1829,21 +1829,19 @@ function inherit_cat_tax_meta($term_id, $tt_id, $taxonomy, $update, $args)
     }
 
     $cat_meta = get_term_meta($term_id);
-    $p_meta = null;
 
     if ($cat_meta['image'][0] && $cat_meta['color'][0] && $cat_meta['cover_photo'][0]) {
         return;
     } else {
         $borrowed_meta = inherit_meta($term_id, 'job_listing_category');
         if ($borrowed_meta) {
-          $p_meta= $borrowed_meta;
             $meta_arr['color'] = $cat_meta['color'] ? $cat_meta['color'] : $borrowed_meta['color'];
             $meta_arr['image'] = $cat_meta['image'] ? $cat_meta['image'] : $borrowed_meta['image'];
             $meta_arr['rl_awesome'] = $cat_meta['rl_awesome'] ? $cat_meta['rl_awesome'] : $borrowed_meta['rl_awesome'];
         }
     }
 
-    $total_meta = array_replace_recursive($cat_meta, $meta_arr);
+    //$total_meta = array_replace_recursive($cat_meta, $meta_arr);
 
 
     remove_action('saved_term', 'inherit_cat_tax_meta');
@@ -1857,7 +1855,6 @@ function inherit_cat_tax_meta($term_id, $tt_id, $taxonomy, $update, $args)
     if ($meta_arr['rl_awesome'][0]) {
         update_term_meta($term_id, 'rl_awesome', $meta_arr['rl_awesome'][0]);
     }
-    update_term_meta($term_id, 'p_meta', $p_meta);
 
     add_action('saved_term', 'inherit_cat_tax_meta');
 }
