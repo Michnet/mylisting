2b1db50a83e674b4ff944fc25c249583aedaf374
Michnet <ntumichael@gmail.com>
2023-12-07 14:47:52 +0300

    gs

--- templates/add-listing/form-fields/event-program-field.php
+++ templates/add-listing/form-fields/event-program-field.php
@@ -87,11 +87,11 @@ if ( $uploaded_files ) {
 					<label>Event day number
 						<span>Which day of the event is this program item scheduled on?</span>
 					</label>
-					<input type="number" name="event_day"
+					<!-- <input type="number" name="event_day"
 						min="1"
 						max="100"
 						step="1"
-						>
+						> -->
 				</div>
 				<div class="form-group col-12">
 					<label>Title </label>
