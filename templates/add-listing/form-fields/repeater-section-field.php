6a8d82d93b0d19f1a7a5e9bf12eeac49a8875fa0
Michnet <ntumichael@gmail.com>
2023-12-08 20:09:16 +0300

    no-rep

--- templates/add-listing/form-fields/repeater-section-field.php
+++ templates/add-listing/form-fields/repeater-section-field.php
@@ -33,68 +33,7 @@ if ( $uploaded_files ) {
 ?>
 
 <div class="resturant-menu-repeater" data-uploaded-list="<?php echo htmlspecialchars(json_encode(! empty( $files ) ? $files : []), ENT_QUOTES, 'UTF-8') ?>" data-list="<?php echo htmlspecialchars(json_encode( isset( $field['value'] ) ? $uploaded_files : []), ENT_QUOTES, 'UTF-8') ?>">
-			
-	<div data-repeater-list="<?php echo esc_attr( (isset($field['name']) ? $field['name'] : $key) ) ?>">
-			
-		<div data-repeater-item class="repeater-field-wrapper">
-			
-		<div class="field-type-file form-group">
-					<div class="field ">	
-						<label>Photo</label>
-						<?php if ( is_admin() ) : ?>
-							<div class="file-upload-field single-upload form-group-review-gallery">
-								<div class="uploaded-files-list review-gallery-images">
-									<div class="upload-file review-gallery-add listing-file-upload-input" data-name="mylisting_accordion_photo" data-multiple="">
-										<i class="mi file_upload"></i>
-										<div class="content"></div>
-									</div>
-									<input type="hidden" class="input-text outer-photo" name="mylisting_accordion_photo">
-									<div class="job-manager-uploaded-files">
-									</div>
-								</div>
-								<small class="description">
-									<?php printf( _x( 'Maximum file size: %s.', 'Add listing form', 'my-listing' ), size_format( wp_max_upload_size() ) ); ?>
-								</small>
-							</div>
-						<?php else : ?>
-							<div class="file-upload-field single-upload form-group-review-gallery ajax-upload">
-								<input
-								type="file"
-								class="input-text review-gallery-input wp-job-manager-file-upload"
-								data-file_types="jpg|jpeg|jpe|gif|png|bmp|tiff|tif|webp|ico|heic"
-								name="mylisting_accordion_photo"
-								id="<?php echo esc_attr( (isset($field['name']) ? $field['name'] : $key) ) ?>_mylisting_accordion_photo"
-								style="display: none;"
-								>
-								<div class="uploaded-files-list review-gallery-images">
-									<label class="upload-file review-gallery-add" for="mylisting_accordion_photo">
-										<i class="mi file_upload"></i>
-										<div class="content"></div>
-									</label>
-
-									<div class="job-manager-uploaded-files">
-									</div>
-								</div>
-
-								<small class="description">
-									<?php printf( _x( 'Maximum file size: %s.', 'Add listing form', 'my-listing' ), size_format( wp_max_upload_size() ) ); ?>
-								</small>
-							</div>
-						<?php endif; ?>
-					</div>
-				</div>
-
-
-			<div class="fields-box row mx-0">
-
-				<div class="form-group w50 col-md-6 col-12">
-					<label>Test name </label>
-					<input required type="text" name="test_name" placeholder="<?php esc_attr_e( 'First Name', 'my-listing' ) ?>">
-				</div>
-			</div>
-			<button data-repeater-delete type="button" aria-label="<?php echo esc_attr( _ex( 'Delete repeater item', 'Repeater field -> Delete item', 'my-listing' ) ) ?>" class="delete-repeater-item buttons button-5 icon-only small"><i class="material-icons delete"></i></button>
-		</div>
-	</div>
+	
 	<div class="item-head form-group">
 				<input type="text" name="title" placeholder="<?php esc_attr_e( 'Section Title', 'my-listing' ) ?>">
 			
