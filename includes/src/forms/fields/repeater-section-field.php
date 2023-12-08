7b2238b1b0e31ddb395d252f0fdbd2e62ba6c3b3
Michnet <ntumichael@gmail.com>
2023-12-08 19:33:11 +0300

    ss

--- includes/src/forms/fields/repeater-section-field.php
+++ includes/src/forms/fields/repeater-section-field.php
@@ -58,7 +58,7 @@ class Repeater_Section_Field extends Base_Field {
 			'title' => sanitize_text_field( $title ),
 			'subtitle' => sanitize_text_field( $posted['sub_title'] ),
 			'descript' => sanitize_text_field( $posted['descript'] ),
-			'list' => $files
+			'list' => $value
 		];
 
 		return $data;
